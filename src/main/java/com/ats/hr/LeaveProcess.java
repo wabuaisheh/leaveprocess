4635147a77b1fcc74985e5f4ab29ce631bb86ee2
wabuaisheh <32914077+wabuaisheh@users.noreply.github.com>
2017-11-02 12:55:31 +0200

    Add files via upload

---
+++ src/main/java/com/ats/hr/LeaveProcess.java
@@ -0,0 +1,8 @@
+package com.ats.hr;
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication
+public class LeaveProcess extends ServletProcessApplication {
+
+}
